ef9308a369d84b982e474da32f1b9705e6d7af65
fartoot <fartoot1337@gmail.com>
2024-09-12 03:41:51 +0100

    improve functionality of navbar and added logos

--- resources/views/layouts/authui.blade.php
+++ resources/views/layouts/authui.blade.php
@@ -17,7 +17,7 @@
     <body class="font-sans text-gray-900 h-full">
         <div class="flex  flex-col  h-screen justify-center px-6 py-12 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-              <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+              <img class="mx-auto h-10 w-auto" src="/img/logo-dark.png" alt="Your Company">
               <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
             </div>
           
