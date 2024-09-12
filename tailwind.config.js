2b8b9b213292f7b4bd85213409406c0b91df5c09
fartoot <fartoot1337@gmail.com>
2024-09-12 06:04:12 +0100

    vercel config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ import forms from '@tailwindcss/forms';
 export default {
     darkMode: 'class',
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        // './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         // './storage/framework/views/*.php',
-        // './resources/views/**/*.blade.php',
+        './resources/views/**/*.blade.php',
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         // "./resources/**/*.vue",
